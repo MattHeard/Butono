653b0949881df3caa1d4dd0512933e0de7a6c5ea
Matthew Heard <matt@mattheard.net>
2019-04-28 21:19:38 +0200

    Replace app icon

--- app/src/test/java/net/mattheard/dogbotface/ExampleUnitTest.java
+++ app/src/test/java/net/mattheard/dogbotface/ExampleUnitTest.java
@@ -1,8 +1,8 @@
-package net.mattheard.salutontero;
+package net.mattheard.dogbotface;
 
 import org.junit.Test;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
 
 /**
  * Example local unit test, which will execute on the development machine (host).
